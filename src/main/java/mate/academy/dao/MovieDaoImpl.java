11df7bff44982e6b3e7b6720aeb951b5b6c4d157
KLICKICE <ledmaskiight@gmail.com>
2025-06-29 21:05:38 +0300

    Fixed: HibernateUtil added private constructor, and MovieDao exception fixed

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.dao;
 
 import java.util.Optional;
+import mate.academy.exception.DataProcessingException;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
@@ -10,13 +11,17 @@ import org.hibernate.Transaction;
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(movie);
-        transaction.commit();
-        session.close();
-        return movie;
+        try {
+            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+            session.close();
+            return movie;
+        } catch (Exception e) {
+            throw new DataProcessingException("Failed to add movie " + movie, e);
+        }
     }
 
     @Override
